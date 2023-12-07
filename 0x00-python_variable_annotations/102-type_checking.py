41f7d22f0d9cdc5af1601784fdeab0e3ca013b6d
Mohamed Amine <ma.abahmane@gmail.com>
2023-12-07 17:27:55 +0100

    Push Advanced Tasks

---
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+Use mypy to validate the following piece of code and apply necessary changes:
+{'lst': typing.Tuple, 'factor': <class 'int'>, 'return': typing.List}
+"""
+
+from typing import Tuple, List
+
+
+def zoom_array(lst: Tuple, factor: int = 2) -> List:
+    zoomed_in: Tuple = tuple([item for i in range(factor) for item in lst])
+    return list(zoomed_in)
+
+
+array = tuple([12, 72, 91])
+
+zoom_2x = zoom_array(array)
+
+zoom_3x = zoom_array(array, int(3.0))
