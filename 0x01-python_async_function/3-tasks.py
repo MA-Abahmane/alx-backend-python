02241efc713429e1b84436917f25ae40dc9d6bc1
Mohamed Amine <ma.abahmane@gmail.com>
2023-12-12 11:22:34 +0100

    Push all Tasks

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -16,3 +16,4 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 def task_wait_random(max_delay: int) -> asy.Task:
     """ A function that takes an integer and returns a asyncio.Task """
     return asy.Task(wait_random(max_delay))
+
